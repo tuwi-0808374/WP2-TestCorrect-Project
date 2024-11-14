b2a8f7d1fd4c06e14645aaf60811491945daba21
tuwi-0808374 <0808374@hr.nl>
2024-11-14 11:54:08 +0100

    terug geven 1 user uit database

--- app.py
+++ app.py
@@ -1,27 +1,41 @@
 from flask import Flask, render_template, session
 
-from lib.model.user import *
+from model.user import *
 
 app = Flask(__name__)
 app.secret_key = "geheime_sleutel"
 
 @app.route('/')
 def home_page():
-    session['logged_user'] = {'name': 'test', 'admin': 1}
     return "<p>Home Page <br></p><p><a href="'/list_users'">list users</a></p>"
 
 @app.route('/list_users')
 def list_user():
-    if 'logged_user' not in session:
-        return "<p>U bent niet ingelogt</p>"
-
-    elif session['logged_user']['admin'] == 0:
-        return "<p>U bent wel ingelogd, maar heeft niet de juiste rechten</p>"
-
-    else:
+    if check_user_is_admin():
         user_model = User()
         all_users = user_model.get_users()
         return render_template("user_list.html", all_users = all_users)
+    else:
+        return "Niet ingelogd of geen admin"
+
+@app.route('/edit_user')
+def edit_user():
+    if check_user_is_admin():
+        user_model = User()
+        all_users = user_model.get_users()
+        return render_template("edit_user.html", sin = all_users)
+    else:
+        return "Niet ingelogd of geen admin"
+
+def check_user_is_admin():
+    session['logged_user'] = {'name': 'test', 'admin': 1}  # test
+
+    if 'logged_user' not in session:
+        return False
+    elif session['logged_user']['admin'] == 0:
+        return False
+
+    return True
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ model/user.py
@@ -0,0 +1,14 @@
+from model.database import Database
+
+class User():
+    def __init__(self):
+        database = Database('./databases/database.db')
+        self.cursor, self.con = database.connect_db()
+
+    def get_users(self):
+        result = self.cursor.execute('SELECT * FROM users').fetchall()
+        return result
+
+    def get_user(self, user_id):
+        result = self.cursor.execute("SELECT * FROM users WHERE user_id = ?", (str(user_id))).fetchone()
+        return result
