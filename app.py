dd291a1e07ab21b75a8778751dacca9c33b5b468
tuwi-0808374 <0808374@hr.nl>
2024-11-14 10:47:42 +0100

    session toegevoegd nodig om na het inloggen om te checken of de redacteur admin is

--- app.py
+++ app.py
@@ -1,18 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session
 
 from lib.model.user import *
 
 app = Flask(__name__)
+app.secret_key = "geheime_sleutel"
 
 @app.route('/')
 def home_page():
-    return "<p>Home Page</p><p><a href="'/list_users'">list users</a></p>"
+    session['logged_user'] = {'name': 'test', 'admin': 1}
+    return "<p>Home Page <br></p><p><a href="'/list_users'">list users</a></p>"
 
 @app.route('/list_users')
 def list_user():
-    user_model = User()
-    all_users = user_model.get_users()
-    return render_template("user_list.html", all_users = all_users)
+    if 'logged_user' not in session:
+        return "<p>U bent niet ingelogt</p>"
+
+    elif session['logged_user']['admin'] == 0:
+        return "<p>U bent wel ingelogd, maar heeft niet de juiste rechten</p>"
+
+    else:
+        user_model = User()
+        all_users = user_model.get_users()
+        return render_template("user_list.html", all_users = all_users)
 
 if __name__ == "__main__":
     app.run(debug=True)
