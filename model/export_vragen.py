aa8a8ae9e728db44a5316b34038b7a2415b012a9
tuwi <0808374@hr.nl>
2024-12-01 10:18:07 +0100

    export/return json back to user;

--- model/export_vragen.py
+++ model/export_vragen.py
@@ -1,11 +1,25 @@
 from model.database import Database
+from flask import jsonify
 
-def export_alle_vragen():
+def export_alle_vragen(save):
     database = Database('./databases/database.db')
     cursor, conn = database.connect_db()
-    select_query = "SELECT questions_id, prompts_id, user_id, question, date_created FROM questions"
+    select_query = "SELECT questions_id, prompts_id, user_id, question, taxonomy_bloom, rtti, date_created FROM questions"
 
     cursor.execute(select_query)
     rows = cursor.fetchall()
+
+    data = []
     for row in rows:
-        print(row['question'])
+        dictionary = {}
+        dictionary['questions_id'] = row['questions_id']
+        dictionary['question'] = row['question']
+        dictionary['taxonomy_bloom'] = row['taxonomy_bloom']
+        dictionary['rtti'] = row['rtti']
+        data.append(dictionary)
+
+    response = jsonify(data)
+
+    return response
+
+
