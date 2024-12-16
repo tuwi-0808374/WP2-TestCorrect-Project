a47ad766852aa0cc12eaf6aa4e36a6b30a97493d
Tim-1100502 <Tim.kolk@Hotmail.com>
2024-12-16 14:20:38 +0100

    Added gpt_model_map to html

--- model/index_page.py
+++ model/index_page.py
@@ -2,9 +2,10 @@ import json
 
 from flask import render_template
 
+from lib.gpt.bloom_taxonomy import gpt_model_map
 from model.import_database import get_question
 
 def index_question(question_id):
     question =  get_question(question_id)
 
-    return render_template('index_page.html', question=question)
+    return render_template('index_page.html', question=question, models=gpt_model_map)
