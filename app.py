be96a26d0a166e49aecbba4bcbcf33819517fb63
Tim-1100502 <Tim.kolk@Hotmail.com>
2025-01-15 16:17:09 +0100

    main function to end of file to ensure routing works

--- app.py
+++ app.py
@@ -21,9 +21,6 @@ app.secret_key = "geheime_sleutel"
 def home_page():
    return render_template('home_page.html')
 
-if __name__ == '__main__':
-    app.run(debug=True)
-
 
 @app.route('/list_users', methods=['GET', 'POST'])
 def list_user():
